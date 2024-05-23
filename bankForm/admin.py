27c23fb3c8da86a96e23c8cbf98876db51b9a4cc
Sahand Sojoodi <sahand@sojoodi.com>
2024-05-23 15:01:33 -0400

    Django Admin for the 3 models.

--- bankForm/admin.py
+++ bankForm/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, AccountInfo, CustomerDetails
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+class AccountInfoAdmin(admin.ModelAdmin):
+    pass
+
+class CustomerDetailsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(AccountInfo, AccountInfoAdmin)
+admin.site.register(CustomerDetails, CustomerDetailsAdmin)
